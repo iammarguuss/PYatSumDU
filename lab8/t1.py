f4e5cc27fb9f2273a8319ae86f297da32b16cc4e
YelyzavetaKim <160435711+YelyzavetaKim@users.noreply.github.com>
2024-05-07 01:34:39 +0300

    ...

--- lab8/t1.py
+++ lab8/t1.py
@@ -8,5 +8,5 @@ def add_student(group_number, full_name, course, grades):
     }
 
 add_student('IN-23', 'Mark Shaposhnik', 2, [9,8,7,8,9,8,7,6,9,10]) #First dictionary assembly, no comments yet))
-
+add_student('IN-23', 'Kim Yelyzaveta', 6, [4,9,2,7,3,6,4,3,5,7]) #дана інформація не зберігається надійно
 print(students)
