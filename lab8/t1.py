0da04137e024e8fc3b3d7bea4b3c1d93d5fa0221
GrandWanderer <kovalenko.vadim2004@gmail.com>
2024-05-08 10:18:31 +0300

    +KV(code,commnt)

--- lab8/t1.py
+++ lab8/t1.py
@@ -4,7 +4,7 @@ def add_student(group_number, full_name, course, grades):
     students[full_name] = {
         'Group Number': group_number,
         'Course': course,
-        'Grades': grades
+        'Grades': grades #Структура оцінок незручна
     }
 
 add_student('IN-23', 'Mark Shaposhnik', 2, [9,8,7,8,9,8,7,6,9,10]) #First dictionary assembly, no comments yet))
